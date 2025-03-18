c4ec4049fd432e0c7e13ef9d81990c4d9033ea35
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-03-18 23:28:11 +0530

    Created the sidebar and header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Chart from './componetns/chart';
 import  Sidebar  from './componetns/sidebar/sidebar';
+import Header from './componetns/header';
 import './App.css'
 function App() {
   return (
     <div className="flex flex-col">
       <Sidebar />
+      <Header/>
       <div className="pl-22 pt-5 pr-5 pb-10">
         <Chart />
       </div>
