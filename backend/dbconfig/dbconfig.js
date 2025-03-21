027e5e40a1173bd4b55f2935ee4de339006ee8a0
Devansh Mishra <115351387+dkmishra2407@users.noreply.github.com>
2025-03-22 00:00:45 +0530

    Changed The DB Connection

--- backend/dbconfig/dbconfig.js
+++ backend/dbconfig/dbconfig.js
@@ -7,3 +7,37 @@ var db = mongoose.connection;
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// const mongoose = require('mongoose');
+
+// // Connection URI (replace <db_password> with your actual password)
+// const mongoDB = 'mongodb+srv://dkmishra2701:<db_password>@growwup.hpjew.mongodb.net/?retryWrites=true&w=majority&appName=GrowwUp';
+
+// // Updated connection options (useNewUrlParser and useUnifiedTopology are no longer needed in newer versions)
+// mongoose.connect(mongoDB)
+//   .then(() => {
+//     console.log('Successfully connected to MongoDB Atlas');
+//   })
+//   .catch(err => {
+//     console.error('MongoDB connection error:', err);
+//     process.exit(1); // Exit with failure code
+//   });
+
+// // Get the default connection
+// const db = mongoose.connection;
+
+// // Additional event listeners for MongoDB connection
+// db.on('error', (err) => {
+//   console.error('MongoDB connection error:', err);
+// });
+
+// db.on('disconnected', () => {
+//   console.log('MongoDB disconnected');
+// });
+
+// db.on('reconnected', () => {
+//   console.log('MongoDB reconnected');
+// });
+
+// // Export the connection
+// module.exports = db;
