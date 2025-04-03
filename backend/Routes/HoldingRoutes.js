3bebad3ade0d74769ee5dd301d72dd9db768cefa
Hitesh Pawar <135259276+hiteshp28@users.noreply.github.com>
2025-04-03 10:20:55 +0530

    Repaired Holding

--- backend/Routes/HoldingRoutes.js
+++ backend/Routes/HoldingRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const  holdingController  = require('../Controller/HoldingController');
 
 router.get('/getholding', holdingController.getHoldings);
-router.post('/addholding', holdingController.addHolding);
-router.delete('/remove', holdingController.removeHolding);
+// router.post('/addholding', holdingController.addHolding);
+// router.delete('/remove', holdingController.removeHolding);
 
 module.exports = router;
